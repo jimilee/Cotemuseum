777b2772c02f30c3e0487a44b41e29f122d1b0f5
1234567890jm48 <1234567890jm48@gmail.com>
2021-09-07 00:51:03 +0900

    0906

--- solved/5_Bronze/2309_일곱_난쟁이.py
+++ solved/5_Bronze/2309_일곱_난쟁이.py
@@ -1,11 +1,28 @@
 import sys
-sys.stdin = open('../../input.txt', 'r')
+sys.stdin = open('./input.txt', 'r')
 
 seven = [int(sys.stdin.readline()) for _ in range(9)]
 sum = 0
 real_seven = [0]*7
 visited = [False]*9
 result = []
+
+def perm(start, cnt):
+    if cnt == 7:
+        print('순열 끝')
+        for k in real_seven:
+            result.append(seven[k])
+        result.sort()
+        for _ in result:
+            print(_)
+        exit(0)
+        return
+    for i in range(start, 9):
+        real_seven[cnt] = i
+        perm(i+1, cnt+1)
+
+perm(0,0)
+
 def comb(cnt, sum):
     if cnt == 7:
         if sum == 100:
@@ -23,5 +40,5 @@ def comb(cnt, sum):
         comb(cnt+1, sum+seven[i])
         visited[i] = False
 
-comb(0,0)
+#comb(0,0)
 
